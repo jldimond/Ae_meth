8bde159c1c64a5f0ffa98e688b6234c96749171a
Jay Dimond <jldimond@uw.edu>
2021-01-28 09:22:03 -0800

    updated analysis

--- scripts/Meth_analysis.R
+++ scripts/Meth_analysis.R
@@ -1,8 +1,7 @@
 setwd("~/Documents/Projects/Anthopleura/Methylation-splicing/Ae_meth/analyses/")
 
-library(GenomicRanges)
-library(rtracklayer)
 library(ggplot2)
+library(factoextra)
 
 #import methylation files produced by nanopolish with threshold of 3 CpG counts per locus
 lst <- list.files(path="~/Documents/Projects/Anthopleura/Methylation-splicing/Ae_meth/analyses/", 
@@ -28,34 +27,15 @@ meth_means <- colMeans(subset(mergedData2, select = c(meth_freq_bar1,meth_freq_b
                                                       meth_freq_bar2,meth_freq_bar4,meth_freq_bar6)))
 plot(meth_means)
 
-asym_mean <- mean(meth_means[1:3])
-sym_mean <- mean(meth_means[4:6])
-
-asym_sd <- sd(meth_means[1:3])
-sym_sd <- sd(meth_means[4:6])
-
-st.err <- function(x) {
-  sd(x)/sqrt(length(x))
-}
-
-asym_se <- st.err(meth_means[1:3])
-sym_se <- st.err(meth_means[4:6])
+meth_means2 <- as.data.frame(cbind(meth_means, c("Aposymbiotic", "Aposymbiotic", "Aposymbiotic", "Symbiotic",
+           "Symbiotic", "Symbiotic")))
+meth_means2$meth_means <- as.numeric(as.character((meth_means2$meth_means)))
 
 
-summary_meth <- as.data.frame(cbind(c("Aposymbiotic", "Symbiotic"), c(asym_mean, sym_mean), c(asym_se, sym_se)))
-summary_meth$V2 <- as.numeric(as.character(summary_meth$V2))
-summary_meth$V3 <- as.numeric(as.character(summary_meth$V3))
-
-
-dodge <- position_dodge(width = 0.9)
-limits <- aes(ymax = summary_meth$V2 + summary_meth$V3,
-              ymin = summary_meth$V2 - summary_meth$V3)
-
-p <- ggplot(data = summary_meth, aes(x = V1, y = V2, fill = V1)) +
-  geom_bar(stat = "identity", position = dodge, fill = c("#DE77AE", "#7FBC41")) +
-  geom_errorbar(limits, position = dodge, width = 0.25) +
-  scale_y_continuous(limits = c(0, NA),
-                     expand = expansion(mult = c(0, 0.1)))+
+#boxplot of overall methylation frequency
+p <- ggplot(meth_means2, aes(x = V2, y = meth_means, fill = meth_means)) +
+  geom_boxplot(fill = c("#DE77AE", "#7FBC41")) +
+  geom_text(aes(fontface=3), x=1, y=0.06, label="p = 0.073") +
   theme_bw() +
   labs(x="",y="Methylation frequency") +
   theme(legend.position = "none")  +
@@ -63,7 +43,7 @@ p <- ggplot(data = summary_meth, aes(x = V1, y = V2, fill = V1)) +
   theme(axis.title = element_text(size = 12, color = "black")) +
   theme(axis.ticks = element_line(color = "black")) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  theme(panel.border = element_rect(size = 1)) 
+  theme(panel.border = element_rect(size = 1)) +
   theme(text = element_text(size = 20)) 
 
 
@@ -73,6 +53,7 @@ t.test(meth_means[1:3], meth_means[4:6], paired = FALSE)
 
 #get only meth freq columns
 meth_cols <- mergedData2[,c(7,11,15,19,23,27)]
+row.names(meth_cols) <- mergedData2$chrom
 
 #ttest cannot deal with all zeros, so find these instances and remove them
 indx <- !rowSums(meth_cols[,1]==meth_cols)==ncol(meth_cols)
@@ -113,11 +94,17 @@ scores <- data.frame(pca$rotation)
 scores2 <- cbind(c("Apo", "Apo", "Apo", "Sym", "Sym", "Sym"), scores)
 colnames(scores2)[1] <- "SymState"
 
+#scree plot
+eig <- fviz_eig(pca, geom = "bar", barfill = "white", barcolor = "black",
+                ggtheme = theme_classic(), main= "", ylab= "% Variance")
+
+
 #component loadings
 (pca$sdev)^2 / sum(pca$sdev^2) 
 # Or cumulative 
 cumsum((pca$sdev)^2) / sum(pca$sdev^2) 
 
+#plot the pca
 pcaplot <- ggplot(scores2, aes(PC1, PC2, colour = SymState)) + 
   theme_bw() +
   geom_point(size = 5) +
@@ -129,27 +116,18 @@ pcaplot <- ggplot(scores2, aes(PC1, PC2, colour = SymState)) +
   theme(axis.title = element_text(size = 12, color = "black")) +
   theme(axis.ticks = element_line(color = "black")) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  theme(panel.border = element_rect(size = 1)) 
+  theme(panel.border = element_rect(size = 1)) +
   theme(text = element_text(size = 20)) 
   
 pcaplot
 
+#add scree plot
+pcaplot2 <- pcaplot +
+  annotation_custom(ggplotGrob(eig), 
+                    ymin = 0.30, ymax= 0.9, xmin=0.405, xmax=0.425)
+
 #plot barplot and PCA together
 library(cowplot)
-plot_grid(p, pcaplot, labels=c("A", "B"), ncol = 2, nrow = 1)
+plot_grid(p, pcaplot2, labels=c("A", "B"), ncol = 2, nrow = 1)
 
-
-#ctg1814:1-2500 no exonerate hits, no mapped ONT seq, 1 mapped Aele seq
-#ctg2526:400-2000 no exonerate hits, no mapped ONT seq, 7 mapped Aele seq
-#ctg1857:8000-11000 one mapped ONT seq AIPGENE7907	TrEMBL	tr|A0A015K5V9|A0A015K5V9_9GLOM	Pif1p, AIPGENE22229	Swiss-Prot	sp|Q9UUA2|PIF1_SCHPO, AIPGENE26387	Swiss-Prot	sp|Q5AXT5|PIF1_EMENI, AIPGENE23631	TrEMBL	tr|A7SKZ0|A7SKZ0_NEMVE	Predicted protein, AIPGENE25249	Swiss-Prot	sp|Q6AZB8|HARB1_DANRE
-#ctg2890:14000-15500 no mapped ONT seq, no exonerate hits, no mapped Aele seq
-#*ctg107:105500-107500 several mapped seq, protein SFI1 homolog [Exaptaisia pallida] 9e-12, 35.85% ident
-#**ctg1212:11000-25000 no mapped ONT reads, AIPGENE15872	Swiss-Prot	sp|Q61493|DPOLZ_MOUSE, DNA polymerase zeta catalytic subunit-like [Exaiptasia pallida], 4e-06, 54.9% ident, DNA polymerase zeta catalytic subunit-like isoform [Acropora millepora], 5e-138, 71.43% ident 
-#*ctg139:129000-131000 AIPGENE21864	Swiss-Prot	sp|Q6AYY8|ACATN_RAT, acetyl-coenzyme A transporter 1 [Exaiptasia pallida] 9e-47, 41.96% ident
-#ctg882:23000-24000 ignored, no exonerate hits
-#ctg662:114000-116000 no exonerate hits, no ONT mapped seq, transmembrane protease serine 9 [Exaiptasia pallida], 6e-05, 95.83 ident; last sequence: splicing factor U2AF 65 kDa subunit [Exaiptasia pallida], 2e-98, 86.5& ident 
-#ctg559:49000-52000 AIPGENE20105	Swiss-Prot	sp|O43299|AP5Z1_HUMAN, AP-5 complex subunit seta-1 {Exaiptasia pallida}, 1e-41, 36.22% ident
-#ctg485:44000-45000 many aip genes without hits
-#*ctg4024:3000-8000 no exonerate hits, hits with retrotransposon elements
-#ctg1695:39800-40500 no exonerate hits
 
